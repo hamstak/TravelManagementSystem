4cd594fdd28653402b49ac1a081ac1821c0f066a
Tim Tanasse <tim.tanasse@gmail.com>
2016-05-25 09:43:12 -0700

    Removed pricing from SeatClass Enum

--- src/airline/seats/SeatClass.java
+++ src/airline/seats/SeatClass.java
@@ -4,13 +4,10 @@ package airline.seats;
  * Created by moth on 5/23/16.
  */
 public enum  SeatClass implements Comparable<SeatClass>{
-    economy(75.00, "Economy Class"), first(10000.00, "First Class"),business(100.00, "Business Class");
+    economy("Economy Class"), first("First Class"), business("Business Class");
 
-
-    private double price;
     private String description;
-     SeatClass(double price, String description){
-         this.price = price;
+     SeatClass(String description){
          this.description = description;
      }
 
